eb00e93dc9b70d281dc9cf3a3936d8162579b04f
ythknakamura <ythk.nakamura@gmail.com>
2025-03-18 17:01:18 +0900

    シングルファイルにビルドするように設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import {viteSingleFile} from 'vite-plugin-singlefile';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     outDir: 'docs',
   },
-  plugins: [svelte()],
+  plugins: [svelte(), viteSingleFile()],
 })
